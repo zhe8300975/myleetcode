8030520b5c99e6e1252e8b12c54375180329405c
zhaowencong <zhaowencong@p1.com>
2020-11-09 19:21:12 +0800

    day7

--- src/easy/day1/LeetCode387.java
+++ src/easy/day1/LeetCode387.java
@@ -1,7 +1,7 @@
 package easy.day1;
 
 //给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。你可以假定该字符串只包含小写字母。
-public class LeetCode384 {
+public class LeetCode387 {
     public static void main(String[] args) {
         System.out.println(firstUniqChar("leetcode"));
     }
